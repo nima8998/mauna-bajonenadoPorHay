40fa1e789c5c1fb0a93e0126ad0204005e66f506
Nicolas Mauna <32561375+nima8998@users.noreply.github.com>
2021-04-26 13:15:25 -0300

    se resuelve correctamente la primse con el objeto en ItemDetailContainer

--- src/container/ItemDetailContainer.jsx
+++ src/container/ItemDetailContainer.jsx
@@ -11,28 +11,29 @@ export default function ItemDetailContainer() {
     useEffect(() => {
         let getItem = () =>{
             return new Promise ((resolve, reject)=>{
+                let objeto = 
+                    {
+                        id: 1,
+                        title: 'Pantalon Mutant Black',
+                        categoria: 'Pantalon',
+                        logo: '/img/products/pantalon.jpeg',
+                        stock: 9,
+                        precio: '4.790',
+                        description: {
+                            info: 'Pantalón tipo cargo con cintura elástica y cordón. Con cierre en rodilas desmontable.',
+                            terminacion: 'Terminación con puño elástico',
+                            material: 'Microfibra',
+                            color: 'Negro',
+                        },
+                        talles: {
+                            talle1: 'ancho 41 cm, largo 97 cm',
+                            talle2: 'ancho 44 cm, largo 98 cm',
+                            talle3: 'ancho 47 cm, largo 99 cm',
+                            talle4: 'ancho 50 cm, largo 100 cm',
+                        }
+                    }
                 setTimeout(() => {
-                    resolve(
-                        {
-                            id: 1,
-                            title: 'Pantalon Mutant Black',
-                            categoria: 'Pantalon',
-                            logo: '/img/products/pantalon.jpeg',
-                            stock: 9,
-                            precio: '4.790',
-                            description: {
-                                info: 'Pantalón tipo cargo con cintura elástica y cordón. Con cierre en rodilas desmontable.',
-                                terminacion: 'Terminación con puño elástico',
-                                material: 'Microfibra',
-                                color: 'Negro',
-                            },
-                            talles: {
-                                talle1: 'ancho 41 cm, largo 97 cm',
-                                talle2: 'ancho 44 cm, largo 98 cm',
-                                talle3: 'ancho 47 cm, largo 99 cm',
-                                talle4: 'ancho 50 cm, largo 100 cm',
-                            }
-                        })
+                    resolve(objeto)
                 }, 2000);
             })
         }
